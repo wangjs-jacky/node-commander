96ebf183ad21a728adec6f8a6e70b122d6bfb1b6
所无聊 <2409277719@qq.com>
2022-05-06 19:55:44 +0800

    面向接口编程(表编程驱动)

---
+++ db.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+const dbPath = path.join(__dirname, 'data', "todo");
+module.exports = {
+    readFromFile: function (path = dbPath) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path, { flag: "a+" }, (error, data) => {
+                if (error) {
+                    console.log(error);
+                    return
+                }
+                let list;
+                try {
+                    list = JSON.parse(data.toString())
+                } catch (error) {
+                    list = [];
+                }
+                resolve(list);
+            })
+        })
+    },
+    writeToFile: function (data, path = dbPath) {
+        let dataString;
+        if (typeof data !== "string") {
+            dataString = JSON.stringify(data);
+        } else {
+            dataString = data;
+        }
+        return new Promise((resolve, reject) => {
+            fs.writeFile(path, dataString, err => {
+                if (err) {
+                    reject(err);
+                }
+                resolve();
+                this.catFile();
+            })
+        })
+    },
+    catFile: async function (path = dbPath) {
+        let dbFile = await this.readFromFile()
+        console.log(dbFile);
+    }
+}
--- index.js
+++ index.js
@@ -2,30 +2,10 @@
 const fs = require("fs");
 const path = require("path")
 const dbPath = path.join(__dirname, 'data', "todo")
-console.log("dbPath", dbPath);
+const db = require("./db");
 
-module.exports.add = (title) => {
-    fs.readFile(dbPath, { flag: "a+" }, (error, data) => {
-        if (error) {
-            console.log(error);
-            return
-        }
-        let list;
-        try {
-            list = JSON.parse(data.toString())
-        } catch (error) {
-            list = [];
-        }
-        list.push({
-            title: title,
-            done: false
-        });
-        const string = JSON.stringify(list)
-        fs.writeFile(dbPath, string, (error2) => {
-            if (error2) {
-                console.log(error2);
-                return;
-            }
-        });
-    })
+module.exports.add = async (title) => {
+    const toDoData = await db.readFromFile(dbPath);
+    toDoData.push({ title, done: false });
+    db.writeToFile(toDoData, dbPath);
 }
